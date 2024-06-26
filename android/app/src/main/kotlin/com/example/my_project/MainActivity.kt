0d1ad73ee0eaf0625c11253cf842cbc1a3a92655
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-26 14:20:55 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.dondivinomusic.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page/home_page_widget.dart
@@ -0,0 +1,58 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_web_view.dart';
+import 'package:flutter/material.dart';
+import 'home_page_model.dart';
+export 'home_page_model.dart';
+
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({super.key});
+
+  @override
+  State<HomePageWidget> createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  late HomePageModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => HomePageModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: Colors.black,
+        body: const Align(
+          alignment: AlignmentDirectional(0.0, 0.0),
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0.0, 30.0, 0.0, 0.0),
+            child: FlutterFlowWebView(
+              content: 'https://app.dondivinomusic.com/home/',
+              height: 844.0,
+              verticalScroll: false,
+              horizontalScroll: false,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
